54c840d204ff14043d34bedcbfd09a53f26372a9
Kweny <iamkweny@gmail.com>
2020-04-01 02:49:25 +0800

    fine turning

--- carefree-redis/src/main/java/org/apenk/carefree/redis/CarefreeRedisWrapper.java
+++ carefree-redis/src/main/java/org/apenk/carefree/redis/CarefreeRedisWrapper.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apenk.carefree.redis;
 
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
--- carefree-redis/src/main/java/org/apenk/carefree/redis/archetype/CarefreeRedisArchetypeSerializer.java
+++ carefree-redis/src/main/java/org/apenk/carefree/redis/archetype/CarefreeRedisArchetypeSerializer.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apenk.carefree.redis.archetype;
 
 import org.apenk.carefree.helper.CarefreeClassDeclaration;
